8c1115cf8f91fa90f0a9e7129223e8e93e5071d7
김다은 <softeun92@naver.com>
2021-10-10 13:52:14 +0900

    가게 상세정보 데이터 표시

--- app/src/main/java/com/example/pizzaorderapp_20211010/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211010/ViewStoreDetailActivity.kt
@@ -2,10 +2,23 @@ package com.example.pizzaorderapp_20211010
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.example.pizzaorderapp_20211010.datas.StoreData
+import kotlinx.android.synthetic.main.activity_view_store_detail.*
+import kotlinx.android.synthetic.main.pizza_store_list_item.*
+import kotlinx.android.synthetic.main.pizza_store_list_item.logoImg
 
 class ViewStoreDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
+
+        val storeData = intent.getSerializableExtra("store") as StoreData
+
+        Glide.with(this).load(storeData.logoURL).into(logoImg)
+        storeNameTxt.text = storeData.name
+
+
+
     }
 }
