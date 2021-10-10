64654bd4190391edf4795233dc1394071c2aca28
김다은 <softeun92@naver.com>
2021-10-10 14:02:57 +0900

    전화걸기 누르면 ->  전화 (CALL) 권한 확인   - Tedpermission으로 권한 체크   - 우선 거절 상황만 코딩

--- app/src/main/java/com/example/pizzaorderapp_20211010/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211010/ViewStoreDetailActivity.kt
@@ -1,9 +1,13 @@
 package com.example.pizzaorderapp_20211010
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.example.pizzaorderapp_20211010.datas.StoreData
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
 import kotlinx.android.synthetic.main.pizza_store_list_item.*
 import kotlinx.android.synthetic.main.pizza_store_list_item.logoImg
@@ -16,7 +20,40 @@ class ViewStoreDetailActivity : AppCompatActivity() {
         val storeData = intent.getSerializableExtra("store") as StoreData
 
         Glide.with(this).load(storeData.logoURL).into(logoImg)
-        storeNameTxt.text = storeData.name
+//        storeNameTxt.text = storeData.name
+        phoneNumTxt.text = storeData.phoneNum
+
+        callBtn.setOnClickListener {
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(this@ViewStoreDetailActivity,"권한이 거절되었습니다",Toast.LENGTH_SHORT).show()
+
+                }
+
+
+            }
+
+
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(Manifest.permission.CALL_PHONE)
+                .check()
+
+
+
+
+        }
+
+
+
 
 
 
