7176434fa25235fae3a5a4eec109369008f2aad2
김다은 <softeun92@naver.com>
2021-10-10 13:46:45 +0900

    가게 누르면 상세화면 이동   - Serializable 활용 데이터 첨부

--- app/src/main/java/com/example/pizzaorderapp_20211010/datas/StoreData.kt
+++ app/src/main/java/com/example/pizzaorderapp_20211010/datas/StoreData.kt
@@ -1,10 +1,12 @@
 package com.example.pizzaorderapp_20211010.datas
 
+import java.io.Serializable
+
 class StoreData(
     val name : String,
     val phoneNum : String,
     val logoURL : String
 
 
-) {
+) : Serializable{
 }
